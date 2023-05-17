75e0a0192de21749bb70234fcc48f3141e82fb9b
Max Schmidt <max.schmidt@outlook.de>
2023-05-17 17:30:59 +0200

    test
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- astro/src/utils/payload.ts
+++ astro/src/utils/payload.ts
@@ -1,9 +1,8 @@
 import type { Post } from "../types";
 
-const url =
-  import.meta.env.NODE_ENV === "development"
-    ? "http://payload:3001"
-    : `https://${import.meta.env.PAYLOAD_URL}`;
+const url = import.meta.env.DEV
+  ? "http://payload:3001"
+  : `https://${import.meta.env.PAYLOAD_URL}`;
 
 export const getPosts = async () =>
   (await (await fetch(`${url}/api/posts`)).json()).docs as Post[];
