7afdc0dd92a97a3e02945a57744bfe102fc04e4e
Max Schmidt <max.schmidt@outlook.de>
2023-06-17 10:51:35 +0200

    Update import paths and tsconfig options

--- payload/src/payload.config.ts
+++ payload/src/payload.config.ts
@@ -1,8 +1,8 @@
 import { buildConfig } from "payload/config";
 import path from "path";
-import Posts from "./collections/Posts";
-import Users from "./collections/Users";
-import Media from "./collections/Media";
+import Posts from "src/collections/Posts";
+import Users from "src/collections/Users";
+import Media from "src/collections/Media";
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_URL,
