6c14f1cbad7eade9f97c35343f39352a5f66ce7c
Max Schmidt <max.schmidt@outlook.de>
2023-07-28 17:22:41 +0200

    Use new ViewTransition
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
   build: {
     inlineStylesheets: "auto",
   },
+  experimental: {
+    viewTransitions: true,
+  },
   integrations: [
     tailwind({
       config: {
