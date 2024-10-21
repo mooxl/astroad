8084e9decc49427abf68437cade104dc055a3eef
Max Schmidt <max.schmidt@outlook.de>
2024-10-21 11:34:29 +0200

    wip
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- apps/payload/src/payload.config.ts
+++ apps/payload/src/payload.config.ts
@@ -9,20 +9,20 @@ import { buildConfig } from "payload/config";
 import Users from "./collections/Users";
 
 export default buildConfig({
-	admin: {
-		user: Users.slug,
-		bundler: webpackBundler(),
-	},
-	editor: slateEditor({}),
-	collections: [Users],
-	typescript: {
-		outputFile: path.resolve(__dirname, "../../../types.ts"),
-	},
-	graphQL: {
-		schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
-	},
-	plugins: [payloadCloud()],
-	db: mongooseAdapter({
-		url: process.env.DATABASE_URI,
-	}),
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: slateEditor({}),
+  collections: [Users],
+  typescript: {
+    outputFile: path.resolve(__dirname, "./types.ts"),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+  },
+  plugins: [payloadCloud()],
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
 });
